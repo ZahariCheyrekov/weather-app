2d7a62de61c628b0dcad8733c984a83a83f2c934
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-10 21:32:04 +0300

    Add inputField validation

--- src/js/app.js
+++ src/js/app.js
@@ -5,28 +5,30 @@ const searchIcon = document.querySelector('.search-icon i');
 searchIcon.addEventListener('click', getData);
 
 async function getData() {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputField.value}&units=metric&appid=${key}`;
-    clearInputField();
+    if (inputField.value) {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputField.value}&units=metric&appid=${key}`;
+        clearInputField();
 
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        const { name } = data;
-        const { temp, feels_like, temp_min, temp_max, humidity } = data.main;
-        const { country } = data.sys;
-        const { speed } = data.wind;
-        const { icon, description, main } = data.weather[0];
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            const { name } = data;
+            const { temp, feels_like, temp_min, temp_max, humidity } = data.main;
+            const { country } = data.sys;
+            const { speed } = data.wind;
+            const { icon, description, main } = data.weather[0];
+            document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
 
-    } catch (error) {
-        inputField.value = 'Invalid city name!';
+        } catch (error) {
+            inputField.value = 'Invalid city name!';
 
-        setTimeout(() => {
-            clearInputField();
-        }, 2000);
+            setTimeout(() => {
+                clearInputField();
+            }, 2000);
+        }
+
+        // console.log(name, temp, temp_min, temp_max, feels_like, humidity, country, speed, icon, description, main)
     }
-
-    // console.log(name, temp, temp_min, temp_max, feels_like, humidity, country, speed, icon, description, main)
 }
 
 function clearInputField() {
