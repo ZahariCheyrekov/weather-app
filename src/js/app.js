68d028f8c1e962ea7b4ce2a71070b4dc3e80a654
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-06-10 21:30:48 +0300

    Add try-catch logic

--- src/js/app.js
+++ src/js/app.js
@@ -6,20 +6,26 @@ searchIcon.addEventListener('click', getData);
 
 async function getData() {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputField.value}&units=metric&appid=${key}`;
-    console.log(url);
     clearInputField();
 
-    const response = await fetch(url);
-    const data = await response.json();
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const { name } = data;
+        const { temp, feels_like, temp_min, temp_max, humidity } = data.main;
+        const { country } = data.sys;
+        const { speed } = data.wind;
+        const { icon, description, main } = data.weather[0];
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
 
-    console.log(data)
-    const { name } = data;
-    const { temp, feels_like, temp_min, temp_max, humidity } = data.main;
-    const { country } = data.sys;
-    const { speed } = data.wind;
-    const { icon, description, main } = data.weather[0];
+    } catch (error) {
+        inputField.value = 'Invalid city name!';
 
-    document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+        setTimeout(() => {
+            clearInputField();
+        }, 2000);
+    }
+
     // console.log(name, temp, temp_min, temp_max, feels_like, humidity, country, speed, icon, description, main)
 }
 
